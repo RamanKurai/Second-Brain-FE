aa40a1e0f47b6c1522c32631fd9b3c73e510cd97
Raman Kurai <ramankurai27@gmail.com>
2025-04-02 22:19:19 +0530

    add : routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { Button } from './components/ui/Button'
-import { PlusIcon } from './icons/PlusIcon'
-import { ShareIcon } from './icons/ShareIcon'
-
+import DashBoard from './pages/DashBoard'
+import { SignIn } from './pages/SignIn'
+import { SignUp } from './pages/SignUp'
+import { BrowserRouter , Routes , Route } from 'react-router-dom'
 function App() {
-  return (
-    <>
-    <Button  size='md' variant={'primary'} text={'Share Brain'} startIcon={<ShareIcon size='md' />} />
-    <br/>
-    <Button size='md' variant={'secondary'} text={'Add Content'} startIcon={<PlusIcon size='md' />} />
-    </>
-  )
+  return <BrowserRouter>
+  <Routes>
+    <Route path='/signup' element={<SignUp/>} /> 
+    <Route path='/signin' element={<SignIn/>} /> 
+    <Route path='/dashboard' element={<DashBoard/>} /> 
+  </Routes>
+  </BrowserRouter>
 }
 
 export default App
